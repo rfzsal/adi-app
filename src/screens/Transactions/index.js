4a4cf2fac3d5c2b386578e0badbb0a13d6b7ff77
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-07-01 05:14:01 +0700

    Update transactions page

--- src/screens/Transactions/index.js
+++ src/screens/Transactions/index.js
@@ -1,23 +1,71 @@
-import { ScrollView, StyleSheet } from 'react-native';
-import { Text, Appbar } from 'react-native-paper';
+import React from 'react';
+import { ScrollView, View, StyleSheet } from 'react-native';
+import { Appbar, Text, useTheme, ActivityIndicator } from 'react-native-paper';
 
-const Transactions = ({ route }) => {
+import Divider from '../../components/Divider';
+import { useAuth } from '../../hooks/useAuth';
+import { useTransactions } from '../../hooks/useTransactions';
+import RippleTransaction from './components/RippleTransaction';
+
+const Transactions = ({ route, navigation }) => {
+  const auth = useAuth();
+  const transactions = useTransactions();
+  const { colors } = useTheme();
+
   return (
     <>
       <Appbar.Header>
         <Appbar.Content title={route.name} />
+        {transactions && (
+          <View style={styles.appbarMenuContainer}>
+            <Text
+              onPress={() => navigation.navigate('History')}
+              style={[styles.appBarMenu, { color: colors.background }]}
+            >
+              Riwayat Transaksi
+            </Text>
+          </View>
+        )}
       </Appbar.Header>
 
-      <ScrollView style={styles.mainContainer}>
-        <Text style={styles.centeredText}>{route.name}</Text>
+      <ScrollView>
+        {!transactions && auth.user && (
+          <>
+            <Divider height={16} />
+            <ActivityIndicator />
+          </>
+        )}
+
+        {transactions &&
+          transactions.map((transaction) => {
+            if (transaction.payment.status !== 'Menunggu Pembayaran') {
+              return null;
+            }
+
+            return (
+              <React.Fragment key={transaction.id}>
+                <RippleTransaction
+                  name={transaction.product.name}
+                  price={transaction.product.price}
+                  discount={transaction.product.discount}
+                  timestamp={transaction.payment.createdAt}
+                  status={transaction.payment.status}
+                  onPress={() =>
+                    navigation.navigate('Transaction', { transaction })
+                  }
+                />
+                <Divider line />
+              </React.Fragment>
+            );
+          })}
       </ScrollView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  mainContainer: { padding: 16 },
-  centeredText: { textAlign: 'center' },
+  appbarMenuContainer: { paddingRight: 12 },
+  appBarMenu: { paddingVertical: 4 },
 });
 
 export default Transactions;
