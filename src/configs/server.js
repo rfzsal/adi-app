93f40b2f5a90c14a5c2a034068bd9ba85ee1b60e
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-07-14 06:05:55 +0700

    Update config

--- src/configs/server.js
+++ src/configs/server.js
@@ -6,21 +6,5 @@ export const paymentAPI = (method) => {
   switch (method) {
     case 'create':
       return 'api/v1/payment';
-    case 'cancel':
-      return 'api/v1/payment/cancel';
-  }
-};
-
-export const userAPI = (method) => {
-  switch (method) {
-    case 'save':
-      return 'api/v1/user';
-  }
-};
-
-export const notificationAPI = (method) => {
-  switch (method) {
-    case 'send':
-      return 'api/v1/notification/send';
   }
 };
