4e12cefa1a9e60863329509164035dd515ebc89b
Isal <58117968+rfzsal@users.noreply.github.com>
2022-08-11 05:14:02 +0700

    Update placeholder checkout

--- src/screens/Checkout/components/OrderDetails.js
+++ src/screens/Checkout/components/OrderDetails.js
@@ -1,20 +1,46 @@
 import currency from 'currency.js';
 import propTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import { Text, useTheme } from 'react-native-paper';
 
 const OrderDetails = ({ product }) => {
   const { colors } = useTheme();
+  const [imageLoaded, setImageLoaded] = useState(false);
   const { name, price, variant, image } = product;
 
   const dummyImage = `https://avatars.dicebear.com/api/initials/${name}.png?b=%23${
-    colors.primary.split('#')[1]
+    colors.placeholder.split('#')[1]
   }`;
 
   return (
     <View style={styles.mainContainer}>
-      <Image style={styles.image} source={{ uri: image || dummyImage }} />
+      <View>
+        <Image
+          style={styles.image}
+          source={{ uri: image || dummyImage }}
+          onLoadEnd={() => setImageLoaded(true)}
+        />
+        {!imageLoaded && (
+          <View
+            style={[
+              styles.imagePlaceholder,
+              {
+                backgroundColor: colors.placeholder,
+              },
+            ]}
+          >
+            <Text
+              style={[
+                styles.imagePlaceholderText,
+                { color: colors.placeholderText },
+              ]}
+            >
+              {name}
+            </Text>
+          </View>
+        )}
+      </View>
 
       <View style={styles.detailContainer}>
         <Text numberOfLines={1} style={styles.title}>
@@ -43,6 +69,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   image: { height: 64, width: 64, marginRight: 16 },
+  imagePlaceholder: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 64,
+    width: 64,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imagePlaceholderText: {
+    fontSize: 12,
+    textAlign: 'center',
+  },
   detailContainer: { flex: 1 },
   title: { letterSpacing: 0.4 },
   priceContainer: {
